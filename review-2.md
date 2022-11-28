825c36c8d97a4a523a79ac9cbe638336d008f537
Jnajera <=>
2022-11-28 14:56:49 -0500

    listo 2 project-4

---
+++ review-2.md
@@ -0,0 +1,6 @@
+#¿Qué es mi producto y para que sirve?
+Todo App es una aplicacion para listar tus rutinas diaras donde podras listar editar y borrar todas las planificaciones que relaices en tus actividades diarias.
+#¿Cuáles son las funcionalidades más importantes y porque los usuarios las usarían?
+Listar, editar y borrar tus rutinas diarias teniedo la opcion de marcar como completada.
+#¿Cuáles son las funcionalidades que podrías incluir en el futuro?
+La aplicacion se está preparando para tener la funcionalidad de recordar al usuario sus tareas pendiente por medio de notificaciones para que el usuario pueda siempre cumplir sus objetivos del dia
--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -6,6 +6,9 @@ import { createTask } from '../Hooks/CreateTask';
 function TaskForm() {
     const [title, setTitle] = useState("");
     const [description, setDescription]=useState('');
+    const [validateForm, setValidateForm]= useState({
+        title:undefined,
+    })
    /*  const {createTask} = useContext(TaskContext); */
     const handleSubmit = (e)=>{
         e.preventDefault();
@@ -16,11 +19,14 @@ function TaskForm() {
   return (
     
     <div className="80-w-80 flex flex-col" >
-        
         <form action="" onSubmit={handleSubmit}  className="mx-auto w-1/5 " >
             <h1  className='text-3xl font-bold capitalize py-4' >Todo App</h1>
             <input type="text" placeholder="Add your new todo" name="" id="" 
                 onChange={(e)=>{
+                    setValidateForm({
+                        ...validateForm,
+                        title: e.target.value.length > 4 ? '': 'Title is required'
+                    })
                     setTitle(e.target.value);
                 }}
                 value= {title}
@@ -28,8 +34,10 @@ function TaskForm() {
                 autoFocus
                 className="bg-slate-200 p-3 w-9/12 mb-2" 
             />
+            {validateForm.title && <span>{validateForm.title}</span>}
             <textarea  rows="2" className='bg-slate-200' placeholder='Add your description' onChange={(e)=>{
-                setDescription(e.target.value);
+                
+                setDescription(e.target.value)
             }}></textarea>
             <button className="bg-green-400 text-xl mx-4 px-3 py-3 round-md mt-4 hover:bg-green-300"  type="submit"><GoPlus/></button>
         </form>
